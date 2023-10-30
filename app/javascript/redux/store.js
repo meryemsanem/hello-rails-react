21b10d2e70711be68a2a483ad7434500f930fba2
meryemsanem <meryemsanemkucukbayram@gmail.com>
2023-10-30 22:54:12 +0300

    Update store.js

--- app/javascript/redux/store.js
+++ app/javascript/redux/store.js
@@ -3,7 +3,7 @@ import greetingsReducer from './greetings/greetingsSlice';
 
 const store = configureStore({
   reducer: {
-    messageList: messagesreducer,
+    greetings: greetingsReducer,
   },
 });
 
